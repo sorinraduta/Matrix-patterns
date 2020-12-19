d45994188c962ab1a5cd99a55973bea773f6ba84
Sorin Raduta <raduta.sorin@yahoo.com>
2020-12-19 07:35:55 +0200

    patch: merged fors

--- main.go
+++ main.go
@@ -8,17 +8,11 @@ func main() {
 	type tMatrix [5][5]int
 	var matrix tMatrix
 
-	// Building the matrix
-	for rowIndex, row := range matrix {
-		for columnIndex := range row {
-			matrix[rowIndex][columnIndex] = calculateFormula(rowIndex+1, columnIndex+1, 5, 5)
-		}
-	}
-
-	// Printing the matrix
+	// Building and printing the matrix
 	for rowIndex, row := range matrix {
 		fmt.Printf("\n ____    ____    ____    ____    ____  \n")
 		for columnIndex := range row {
+			matrix[rowIndex][columnIndex] = calculateFormula(rowIndex+1, columnIndex+1, 5, 5)
 			fmt.Printf("| %0.2d |\t", matrix[rowIndex][columnIndex])
 		}
 		fmt.Printf("\n ‾‾‾‾    ‾‾‾‾    ‾‾‾‾    ‾‾‾‾    ‾‾‾‾  ")
